7eef6702b256eba890c2ed4edea32c63eb6c6717
Penumbra <konstantinfedenov@gmail.com>
2025-02-16 21:02:32 +0300

    First commit

---
+++ src/main/java/com/example/restservice/controller/SongController.java
@@ -0,0 +1,42 @@
+package com.example.restservice.controller;
+
+import com.example.restservice.model.Song;
+import com.example.restservice.service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/songs")
+public class SongController {
+
+    private final SongService songService;
+
+    @Autowired
+    public SongController(SongService songService) {
+        this.songService = songService;
+    }
+
+    @GetMapping("/{id}")
+    public Song getSongById(@PathVariable Integer id) {
+        Optional<Song> song = songService.getSong(id);
+        return song.orElse(null);
+    }
+
+    @GetMapping("/info-by-author")
+    public List<Song> getSongsInfoByAuthor(@RequestParam String author) {
+        return songService.getSongsByAuthor(author);
+    }
+
+    @GetMapping("/info-by-song-name")
+    public List<Song> getSongsInfoByName(@RequestParam String name) {
+        return songService.getSongsByName(name);
+    }
+
+    @GetMapping("/info-by-author-and-song-name")
+    public List<Song> searchSongsByAuthorAndName(@RequestParam String author, @RequestParam String name) {
+        return songService.searchSongsByAuthorAndName(author, name);
+    }
+}
---
+++ src/main/java/com/example/restservice/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.restservice.service;
+
+import com.example.restservice.model.User;
+import com.example.restservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    public Optional<User> getUser(Integer id) {
+        return userRepository.getUserById(id);
+    }
+}
