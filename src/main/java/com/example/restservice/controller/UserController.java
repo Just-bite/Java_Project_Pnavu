40e102aeedd26a5f8247ab39721d090edc630136
Penumbra <konstantinfedenov@gmail.com>
2025-02-16 21:02:32 +0300

    First commit

---
+++ src/main/java/com/example/restservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.restservice.controller;
+
+import com.example.restservice.model.User;
+import com.example.restservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Integer id) {
+        return userService.getUser(id).orElse(null);
+    }
+}
---
+++ src/main/java/com/example/restservice/service/SongService.java
@@ -0,0 +1,43 @@
+package com.example.restservice.service;
+
+import com.example.restservice.model.Song;
+import com.example.restservice.repository.SongRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SongService {
+    private final List<Song> songList;
+
+    public SongService(SongRepository songRepository) {
+        this.songList=songRepository.getAllSongs();
+    }
+
+    public Optional<Song> getSong(Integer id) {
+        return songList.stream()
+                .filter(song -> song.getId() == id)
+                .findFirst();
+    }
+
+    public List<Song> getSongsByAuthor(String author) {
+        return songList.stream()
+                .filter(song -> song.getAuthorName().equalsIgnoreCase(author))
+                .toList();
+    }
+
+    public List<Song> getSongsByName(String name) {
+        return songList.stream()
+                .filter(song -> song.getSongName().equalsIgnoreCase(name))
+                .toList();
+    }
+
+    public List<Song> searchSongsByAuthorAndName(String author, String name) {
+        return songList.stream()
+                .filter(song -> song.getAuthorName().equalsIgnoreCase(author) &&
+                        song.getSongName().equalsIgnoreCase(name))
+                .toList();
+    }
+}
+
