209fe4c93a198c8a66a780c01b0828eb3b592e8f
Penumbra <konstantinfedenov@gmail.com>
2025-02-19 01:15:58 +0300

    Checkstyle passed

--- src/main/java/com/example/restservice/controller/SongController.java
+++ src/main/java/com/example/restservice/controller/SongController.java
@@ -5,7 +5,11 @@ import com.example.restservice.service.SongService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/songs")
--- src/main/java/com/example/restservice/controller/UserController.java
+++ src/main/java/com/example/restservice/controller/UserController.java
@@ -4,7 +4,10 @@ import com.example.restservice.model.User;
 import com.example.restservice.service.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
