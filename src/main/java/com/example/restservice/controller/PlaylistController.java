d84719388c95ab7dd6d70e7a8870c47ca691c540
Penumbra <konstantinfedenov@gmail.com>
2025-03-21 23:27:21 +0300

    Swagger operation added to PlaylistController

--- src/main/java/com/example/restservice/controller/PlaylistController.java
+++ src/main/java/com/example/restservice/controller/PlaylistController.java
@@ -2,6 +2,7 @@ package com.example.restservice.controller;
 
 import com.example.restservice.model.Playlist;
 import com.example.restservice.service.PlaylistService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -24,32 +25,56 @@ public class PlaylistController {
     private final PlaylistService playlistService;
 
     @GetMapping
+    @Operation(
+            summary = "Вывод плейлистов",
+            description = "Выводит все плейлисты и информацию о песнях, в них хранящихся"
+    )
     public List<Playlist> getAllPlaylists() {
         return playlistService.getAllPlaylists();
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Возвращает плейлист по id"
+    )
     public Playlist getPlaylistById(@PathVariable Long id) {
         return playlistService.getPlaylistById(id);
     }
 
     @PostMapping("/create/{userId}")
+    @Operation(
+            summary = "Создание плейлиста",
+            description = "Создает пустой плейлист для пользователя с полученым id"
+    )
     public Playlist createPlaylist(@RequestBody Playlist playlist, @PathVariable Long userId) {
         return playlistService.createPlaylist(playlist, userId);
     }
 
     @PostMapping("/{playlistId}/add-songs")
+    @Operation(
+            summary = "Добавление песен в плейлист",
+            description = "Добавляет в  плейлист с полученым id песни"
+    )
     public Playlist addSongsToPlaylist(@PathVariable Long playlistId,
                                        @RequestBody List<Long> songIds) {
         return playlistService.addSongsToPlaylist(playlistId, songIds);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Удаление плейлиста",
+            description = "Удаляет плейлист по его id"
+    )
     public void deletePlaylist(@PathVariable Long id) {
         playlistService.deletePlaylist(id);
     }
 
     @GetMapping("/with-songs")
+    @Operation(
+            summary = "Вывод непустых плейлистов",
+            description = "Выводит все плейлисты и информацию о песнях, в них хранящихся,"
+                        + "если они непустые"
+    )
     public List<Playlist> getPlaylistsWithSongs() {
         return playlistService.getPlaylistsWithSongs();
     }
