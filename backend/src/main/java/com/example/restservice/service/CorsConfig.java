2f12c1deb979abbbce906e360dd0f5d2ecdc9825
Penumbra <konstantinfedenov@gmail.com>
2025-05-12 03:04:03 +0300

    Cors

--- backend/src/main/java/com/example/restservice/service/CorsConfig.java
+++ backend/src/main/java/com/example/restservice/service/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:3000",      // Локальный фронтенд
-                        "https://your-frontend.onrender.com"  // Продакшен-фронтенд
+                        "https://java-project-pnavu-1.onrender.com"  // Продакшен-фронтенд
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
